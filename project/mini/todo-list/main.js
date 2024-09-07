e9ca5da751a202b4a7a885b5d6c260d7a3312844
eunjinim <eunjee2058@gmail.com>
2024-09-07 18:06:03 +0900

    다크모드 기능 만들기와 디자인 완성

--- project/mini/todo-list/main.js
+++ project/mini/todo-list/main.js
@@ -1,9 +1,28 @@
-function keyCodeCheck(event) {
-  console.log(event)
+// function keyCodeCheck(event) {
+//   console.log(event)
 
-  // 엔터를 치면, ul에 할 일을 li로 만들것이다?
-  if (event.key == "Enter") {
+//   // 엔터를 치면, ul에 할 일을 li로 만들것이다?
+//   if (event.key == "Enter") {
+//   }
+// }
+
+// document.querySelector("#todoInput").addEventListener("keydown", keyCodeCheck)
+
+// 다크모드 토글 스위치 이동하는 기능 만들기
+let count = 0
+document.querySelector(".toggle-switch").addEventListener("click", function () {
+  count++
+  if (count % 2 != 0) {
+    document.querySelector(".toggle-switch").style.transform =
+      "translateX(26px)"
+  } else {
+    document.querySelector(".toggle-switch").style.transform = "translateX(0)"
   }
-}
+})
 
-document.querySelector("#todoInput").addEventListener("keydown", keyCodeCheck)
+// 다크모드 눌렀을 때의 디자인
+document.querySelector(".toggle-switch").addEventListener("click", function () {
+  document.querySelector(".section").classList.toggle("type_dark")
+  document.querySelector(".toggle-switch").classList.toggle("type_dark-button")
+  document.querySelector(".add-btn").classList.toggle("type_dark-button")
+})
